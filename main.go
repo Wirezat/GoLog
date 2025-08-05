e966b02a6b498eeda5a6e4e14dc0c1f532790603
Wirezat <dwebste29@gmail.com>
2025-08-05 09:22:40 +0200

    things

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package GoLog
 
 import (
@@ -101,3 +102,128 @@ func Errorf(format string, a ...any) { logToConsoleAndFile(err, format, a...) }
 
 func Debug(msg string)               { logToConsoleAndFile(debug, "%s", msg) }
 func Debugf(format string, a ...any) { logToConsoleAndFile(debug, format, a...) }
+=======
+package GoLog
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+	"time"
+)
+
+type LogLevel int
+
+const (
+	INFO LogLevel = iota
+	WARN
+	ERROR
+)
+
+var (
+	mu         sync.Mutex
+	logLevel   = INFO
+	logger     = log.New(os.Stdout, "", 0)
+	fileLogger *log.Logger
+	logFile    *os.File
+)
+
+// #region Setup
+
+func SetLogLevel(level LogLevel) {
+	mu.Lock()
+	defer mu.Unlock()
+	logLevel = level
+}
+
+// ToFile enables file logging in a dated log file within the default log directory.
+func ToFile() error {
+	executablePath, err := os.Executable()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Could not determine executable path:", err)
+		return err
+	}
+
+	executableName := strings.TrimSuffix(filepath.Base(executablePath), filepath.Ext(executablePath))
+	logDir := filepath.Join(filepath.Dir(executablePath), "logs", executableName)
+
+	if err := ensureDir(logDir); err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to create log directory:", err)
+		return err
+	}
+
+	logFileName := filepath.Join(logDir, time.Now().Format("2006-01-02")+".log")
+	f, err := openLogFile(logFileName)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to open log file:", err)
+		return err
+	}
+
+	if logFile != nil {
+		logFile.Close()
+	}
+	logFile = f
+	fileLogger = log.New(logFile, "", 0)
+
+	fmt.Fprintln(os.Stdout, "File logging enabled:", logFileName)
+	return nil
+}
+
+// ensureDir makes sure a directory exists.
+func ensureDir(path string) error {
+	return os.MkdirAll(path, 0755)
+}
+
+// openLogFile opens or creates a log file for appending.
+func openLogFile(path string) (*os.File, error) {
+	return os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+}
+
+// #endregion
+
+// #region Formatting
+
+func formatMessage(level LogLevel, msg string) string {
+	levelStr := ""
+	switch level {
+	case INFO:
+		levelStr = "INFO"
+	case WARN:
+		levelStr = "WARN"
+	case ERROR:
+		levelStr = "ERROR"
+	default:
+		levelStr = "UNKNOWN"
+	}
+	return fmt.Sprintf("%s [%s] %s", levelStr, time.Now().Format(time.RFC3339), msg)
+}
+
+// #endregion
+
+// #region Logging
+func logToConsoleAndFile(level LogLevel, format string, args ...interface{}) {
+	if level < logLevel {
+		return
+	}
+
+	msg := fmt.Sprintf(format, args...)
+	formatted := formatMessage(level, msg)
+
+	mu.Lock()
+	defer mu.Unlock()
+
+	logger.Println(formatted)
+	if fileLogger != nil {
+		fileLogger.Println(formatted)
+	}
+}
+
+func Info(format string, a ...interface{})  { logToConsoleAndFile(INFO, format, a...) }
+func Warn(format string, a ...interface{})  { logToConsoleAndFile(WARN, format, a...) }
+func Error(format string, a ...interface{}) { logToConsoleAndFile(ERROR, format, a...) }
+
+// #endregion
+>>>>>>> parent of df4e55a (Added Debug Loglevel und F logging. made code simpler)
